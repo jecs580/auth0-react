c03736cf665e315a2d9bae6d6f6157f9288cadab
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-08-14 22:26:32 -0400

    Agregando boton para cerrar sesion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import LoginButton from './components/LoginButton'
+import LogoutButton from './components/LogoutButton';
 import Profile from './components/Profile'
+import { useAuth0 } from '@auth0/auth0-react'
 function App() {
-  
+  const {isAuthenticated} = useAuth0();
   return (
     <div className="App">
       <h1>Aplicación</h1>
-      <LoginButton/>
+      {
+        isAuthenticated?<LogoutButton/>:<LoginButton/>
+      }
       <Profile/>
     </div>
   );
